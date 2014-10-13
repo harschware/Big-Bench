c6720cdcdfc48b3e47036feb899b9a33b8962261
Tim Harsch <tharsch@yarcdata.com>
2014-10-12 17:31:09 -0700

    changed parseBigBench to sort outputs

--- scripts/parseBigBench.pl
+++ scripts/parseBigBench.pl
@@ -33,8 +33,7 @@ my $csv = Text::CSV->new ( { binary => 1, sep_char => ';' } )  # should set bina
                  or dieWithUsage( "Cannot use CSV: ".Text::CSV->error_diag () );
 
 open my $fh, "<:encoding(utf8)", $file or dieWithUsage( "Cannot open '$file': $!" );
-$csv->getline( $fh ); # Toss out column headers and make our own...
-print "Query\tMinutes\n";
+$csv->getline( $fh ); # Toss out column headers and later make our own...
 while ( my $row = $csv->getline( $fh ) ) {
    #for( my $i=0; $i < $#$row; $i++ ) {
    #    print "$i:" . $row->[$i] . "\n";
@@ -53,16 +52,27 @@ while ( my $row = $csv->getline( $fh ) ) {
   
    $label .= "-S" . $row->[2];
    $label .= "-Q" . $row->[3];
-   print $label, "\t";
    my $time = $row->[6];
    $time = $time / ( 60 * 1000 );  # convert milliseconds to minutes
    $time = sprintf "%.2f", $time; # round to two places
-   print $time, "\n";
 
-   push @rows, $row;
+   push @rows, [ $label, $time ]; 
 }
 $csv->eof or $csv->error_diag();
 close $fh;
+
+print "Query\tMinutes\n";
+my @sorted = sort { 
+	my( $aLabel,$aStream,$aQuery ) = $a->[0] =~ /^(.*)-S(\d+)-Q(\d+)/;
+	my( $bLabel,$bStream,$bQuery ) = $b->[0] =~ /^(.*)-S(\d+)-Q(\d+)/;
+
+	my $cmpVal = $aLabel cmp $bLabel || $aStream <=> $bStream || $aQuery <=> $bQuery;
+	return $cmpVal;
+} @rows;
+
+foreach my $sortrow ( @sorted ) {
+	print join( "\t", @$sortrow ) . "\n";
+} # end foreach
 
 ############### END MAIN ###############
 
